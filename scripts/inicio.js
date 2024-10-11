d39a577e06e56d5b30686162b75c1cb3c2dadff4
JPeMANY <jp.hobuss@gmail.com>
2024-10-11 15:51:56 -0300

    Atualizações

---
+++ scripts/inicio.js
@@ -0,0 +1,75 @@
+const titulo = document.querySelector('header .titulo h1');
+const subtitulo = document.querySelector('header .titulo span');
+
+const cartaoRio = document.getElementById('cartaoRio')
+const cartaoIncendio = document.getElementById('cartaoIncendio');
+const cartaoEnchente = document.getElementById('cartaoEnchente');
+const cartaoTempestade = document.getElementById('cartaoTempestade');
+
+const txtIncendio = cartaoIncendio.querySelector('p');
+const txtEnchente = cartaoEnchente.querySelector('p');
+const txtTempestade = cartaoTempestade.querySelector('p');
+
+function calcularRio(altLim, altAt) {
+    const altLimHTML = document.getElementById('valorAltLim');
+    const altAtHTML = document.getElementById('valorAltAt');
+    altLimHTML.textContent = altLim;
+    altAtHTML.textContent = altAt;
+
+    const altLimNumero = Number(altLim.replace(/\D/g, ''));
+    const altAtNumero = Number(altAt.replace(/\D/g, ''));
+
+    // Cálculos
+
+    // retira 3 quartos do valor de altura atual de ambos
+    const desconto = (altAtNumero / 4) * 3;
+    const altLimCalc = altLimNumero - desconto;
+    const altAtCalc = altAtNumero - desconto;
+
+    // Regra de 3
+    const altAtPorcentagem = (altAtCalc * 100) / altLimCalc;
+
+    console.log(altAtPorcentagem)
+
+}
+
+function atualizarInicio() {
+    titulo.textContent = cidade;
+    subtitulo.textContent = regiao;
+
+    const jsonUrl = `dados/${regiao.replace(/\s+/g, '')}/${cidade.replace(/\s+/g, '')}.json?` + new Date().getTime();
+
+    fetch(jsonUrl)
+        .then(response => response.json()) // Converter a resposta para JSON
+        .then(data => {
+            
+            // Armazena os dados em variáveis
+            const incendio = data.incendio;
+            const enchente = data.enchente;
+            const tempestade = data.tempestade;
+
+            const rio = data.rio;
+            const alturaLimite = data.alturaLimite;
+            const alturaAtual = data.alturaAtual;
+
+            // Altera o texto de subtítulo dos cartões
+            txtIncendio.textContent = incendio;
+            txtEnchente.textContent = enchente;
+            txtTempestade.textContent = tempestade;
+
+            // Altera o estilo dos cartões
+            cartaoIncendio.classList.add(incendio.replace(/\s+/g, '').toLowerCase());
+            cartaoEnchente.classList.add(enchente.replace(/\s+/g, '').toLowerCase());
+            cartaoTempestade.classList.add(tempestade.replace(/\s+/g, '').toLowerCase());
+
+            // Estilo cartão rio
+            if (rio === "false") {
+                cartaoRio.style.display = 'none';
+            } else {
+                cartaoRio.style.display = 'block';
+                calcularRio(alturaLimite, alturaAtual);
+            }
+
+        })
+        .catch(error => console.error('Erro ao carregar o arquivo JSON:', error));
+} 
